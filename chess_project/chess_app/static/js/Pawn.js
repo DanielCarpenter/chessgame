b85676835b01a561e725f006dd54944f4366da05
Daniel Carpenter <d_c176@txstate.edu>
2021-04-04 16:32:57 -0500

    Pawn movement

--- chess_project/chess_app/static/js/Pawn.js
+++ chess_project/chess_app/static/js/Pawn.js
@@ -1,26 +1,21 @@
-class Pawn extends Phaser.GameObjects.Sprite {
+class Pawn extends Piece {
     constructor(config) {
-        super(config.scene, config.x, config.y, config.img);
-        config.scene.add.existing(this);
-        this.setInteractive();
-        this.setScale(cell_size / 45);
-        config.scene.input.setDraggable(this);
-        this.color = config.img == "bp" ? "black" : "white";
-
-        this.on('dragstart', function (pointer) {
-            this.x = pointer.x;
-            this.y = pointer.y;
-        });
-
-        this.on('drag', function(pointer, dragX, dragY) {
-            this.x = pointer.x;
-            this.y = pointer.y;
-        });
-
+        super(config);
         //sets sprite on center of closest tile based on pointer location on drop end
+        this.isFirstMove = true;
         this.on('dragend', function(pointer, dragX, dragY) {
-            this.x = Math.round(pointer.x/off_set)*off_set % 100 == 0 ? Math.round(pointer.x/off_set)*off_set + off_set : Math.round(pointer.x/off_set)*off_set;
-            this.y = Math.round(pointer.y/off_set)*off_set % 100 == 0 ? Math.round(pointer.y/off_set)*off_set + off_set : Math.round(pointer.y/off_set)*off_set;
+            console.log(Math.round(pointer.y/off_set)*off_set + 100, this.y )
+            if((((Math.round(pointer.y/off_set)*off_set + 200) == this.prevYcoord && this.isFirstMove) || ((Math.round(pointer.y/off_set)*off_set + 100) == this.prevYcoord)) && (Math.round(pointer.x/off_set)*off_set) == this.prevXcoord) {
+                this.x = Math.round(pointer.x/off_set)*off_set % 100 == 0 ? Math.round(pointer.x/off_set)*off_set + off_set : Math.round(pointer.x/off_set)*off_set;
+                this.y = Math.round(pointer.y/off_set)*off_set % 100 == 0 ? Math.round(pointer.y/off_set)*off_set + off_set : Math.round(pointer.y/off_set)*off_set;
+                this.isFirstMove = false;
+            }
+            else {
+                this.x = this.prevXcoord;
+                this.y = this.prevYcoord;
+            }
+            
+
         });
     }
 }
