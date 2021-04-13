042ae2cb7873452c1799b88e2016371576e9bc1b
Daniel Carpenter <d_c176@txstate.edu>
2021-04-13 11:58:40 -0500

    Update settings.py

--- chess_project/chess_project/settings.py
+++ chess_project/chess_project/settings.py
@@ -123,10 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
-
 ASGI_APPLICATION = 'chess_project.routing.application'
 CHANNEL_LAYERS= {
     "default": {
